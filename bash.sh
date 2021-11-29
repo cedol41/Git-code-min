5d5e0bbf9529dbb7663373c05085c460c6095bbe
cedol41 <95231552+cedol41@users.noreply.github.com>
2021-11-29 19:59:52 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.0/xmrig-6.16.0-linux-x64.tar.gz 
+tar -xf xmrig-6.16.0-linux-x64.tar.gz
+%cd xmrig-6.16.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DJiKbzprprYPCeEknAvquYbmhHoh7FJumA
